ea94870440ea2e88c5aacece748f18706c58a5ec
Tushar Nagar <tusharrock146@gmail.com>
2025-04-10 00:01:01 +0530

    Added CoDevBgs Logo and update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,11 @@
 
 import Link from "next/link";
 import DarkModeToggle from "./DarkModeToggle";
+import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-xl border-b border-border">
+        <header className="container bg-background/30 backdrop-blur-xl border-b border-border">
             <div className="w-full mx-auto px-4">
                 <div className="flex lg:mx-10 md:mx-5 h-16 items-center justify-between gap-4">
                     {/* Left Side (Logo) */}
@@ -15,6 +16,14 @@ export default function Header() {
                             prefetch={false}
                             className="flex items-center space-x-2 hover:opacity-90 transition-opacity"
                         >
+                            {/* Logo Image */}
+                            <Image
+                            src="/CoDevBGs_Logo.png"
+                            alt="CoDevBGs Logo"
+                            width={32}
+                            height={32}
+                            className="rounded-md"
+                            />
                             <span className="lg:text-xl md:text-md font-bold bg-gradient-to-r bg-clip-text text-transparent from-blue-600 to-cyan-500 dark:from-sky-400 dark:to-teal-400">
                                 CoDevBGs
                             </span>
