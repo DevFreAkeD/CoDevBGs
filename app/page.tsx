4b57b94591eed353eb2fe64528720dd537fa40aa
Tushar Nagar <tusharrock146@gmail.com>
2025-04-09 12:54:20 +0530

    pass backgrounds prop to Hero component on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { backgrounds } from "@/lib/constants";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <Header />
 
       <main className="pt-20 container mx-auto max-w-7xl px-4 py-8">
-        <Hero />
+        <Hero backgrounds={backgrounds} />
       </main>
     </div>
   );
